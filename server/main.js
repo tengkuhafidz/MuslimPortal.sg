aad66cddac359e685ca8ef8fa6e631dc0f45bd52
heartziq <heartziq@gmail.com>
2016-08-14 17:14:00 +0800

    added musollah collection

--- server/main.js
+++ server/main.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 //import all collections from api here
 import '../imports/api/events.js';
-
+import '../imports/api/musolla.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
