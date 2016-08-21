239afedde2e68ab5e0d4601730e7d4931dc11003
tengkuhafidz <tengkuhafidz@gmail.com>
2016-08-21 16:36:57 +0800

    nothing significant

--- imports/ui/pages/Home.jsx
+++ imports/ui/pages/Home.jsx
@@ -4,13 +4,19 @@ import Greeting from '../Home/Greeting.jsx'
 
 export default class Home extends React.Component{
 
+  componentDidMount(){
+    console.log("in")
+    $('.swashIn').addClass('magictime puffIn');
+    console.log("out")
+  }
+
   render(){
     var randomMessage = messages[Math.floor(Math.random() * messages.length)];
 
     return(
       <div className="center moreTopPadding ">
         <Greeting />
-        <h1 className="topPadding "> 
+        <h1 className="topPadding swashIn"> 
           {randomMessage.message}
         </h1>
         <div className="flow-text">#{randomMessage.hashtag}</div>
