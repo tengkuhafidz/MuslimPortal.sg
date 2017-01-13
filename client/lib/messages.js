a2344f889ac573bfb1f6c98819c6935f1ac83bfb
tengkuhafidz <tengkuhafidz@gmail.com>
2017-01-13 12:09:45 +0800

    changing bg

--- client/lib/messages.js
+++ client/lib/messages.js
@@ -16,3 +16,9 @@ messages = [
   {"message": "The real patience is at the first stroke of a calamity", "hashtag":"Hadith, Bukhari"}
 
 ]
+
+var totalCount = 5;
+var num = Math.ceil( Math.random() * totalCount );
+// document.body.background = num + ".jpg";
+document.body.style.background = "linear-gradient(rgba(15,109,102, 0.8), rgba(15,109,102, 0.8)), url('/" + num + ".jpg')" ;
+document.body.style.backgroundSize = "cover";
