ede086baf956da6b32180ff339b0c0b787bd906f
tengkuhafidz <tengkuhafidz@gmail.com>
2017-01-14 15:11:37 +0800

    added bebetter page

---
+++ imports/ui/pages/BeBetter.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class BeBetter extends React.Component{
+
+  // componentDidMount(){
+  //     $('.swashIn').addClass('magictime swashIn');
+  // }
+
+  render(){
+    var randomMessage = messages[Math.floor(Math.random() * messages.length)];
+
+    return(
+      <div className="center moreTopPadding ">
+
+        <img src="/logo-white.png" height="80px"/>
+        <h5>#BeBetter Challenge of The Week:</h5>
+        <br/>
+        <h1 className="cursiveFont noTopGap">Start Every Conversation With A Smile</h1>
+          <br/>
+            <br/>
+        <div className=" betaFont mainContent">
+
+        </div>
+      </div>
+    )
+  }
+}
