86e38f54652abd2d7c1563fda18a9ce6fb13eeb7
heartziq <heartziq@gmail.com>
2017-01-14 15:49:32 +0800

    added prayertime display

--- imports/ui/pages/HijrahPrayer.jsx
+++ imports/ui/pages/HijrahPrayer.jsx
@@ -9,18 +9,20 @@ export default class HijrahPrayer extends React.Component{
     super();
 
     this.state = {
-      hijrah: ''
+      hijrah: '',
+      prayer: ''
     }
   }
 
-  componentWillMount() {
-
+  componentDidMount() {
+    this.getHijrahDate();
+    this.getPrayerTime();
   }
 
-  render() {
-    that = this
+  getHijrahDate() {
 
-    HTTP.call( 'GET', 'https://raw.githubusercontent.com/ruqqq/prayertimes-database/master/hijri/2017/SG-1.json', {}, function( error, response ) {
+    that = this
+    HTTP.call('GET', 'https://raw.githubusercontent.com/ruqqq/prayertimes-database/master/hijri/2017/SG-1.json', {}, function( error, response ) {
 
    if (error) {
      console.log(error);
@@ -41,33 +43,100 @@ export default class HijrahPrayer extends React.Component{
        'Zulhijjah': 12,
      };
 
-     var currDate = moment().date();
-     var currMonth = moment().month();
+       var currDate = moment().date();
+       var currMonth = moment().month();
 
-    data = JSON.parse(response.content)
-    // keys = Object.keys(data)
+      data = JSON.parse(response.content)
+      // keys = Object.keys(data)
 
-    //get current month, day, year (hijri)
-    var currHijriMonth = data[0][currDate-1].hijriMonth;
-    var hijriMonthName = Object.keys(HIJRI_MONTHS)[currHijriMonth-1];
+      //get current month, day, year (hijri)
+      var currHijriMonth = data[0][currDate-1].hijriMonth;
+      var hijriMonthName = Object.keys(HIJRI_MONTHS)[currHijriMonth-1];
 
-    var hijriDate = data[0][currDate-1].hijriDate;
-    var hijriYear = data[0][currDate-1].hijriYear;
+      var hijriDate = data[0][currDate-1].hijriDate;
+      var hijriYear = data[0][currDate-1].hijriYear;
 
-    var hDate = `Today is ${hijriDate}, ${hijriMonthName} ${hijriYear}`;
+      var hDate = `Today is ${hijriDate}, ${hijriMonthName} ${hijriYear}`;
 
      that.setState({
        hijrah: hDate
      })
-
    }
- })
+    })
 
- return (
-   <div>
-     <h1>{this.state.hijrah}</h1>
-   </div>
- )
+  }
+
+  getPrayerTime() {
+    that = this
+    HTTP.call('GET', 'https://raw.githubusercontent.com/ruqqq/prayertimes-database/master/data/SG/1/2017.json', {}, function( error, response ) {
+
+   if (error) {
+     console.log(error);
+   } else {
+
+     const PRAYER = {
+       'Subuh': 0,
+       'Syuruk': 1,
+       'Zuhur': 2,
+       'Asar': 3,
+       'Maghrib': 4,
+       'Isyak': 5
+     }
+
+       var currDate = moment().date();
+       var currMonth = moment().month();
+
+       data = JSON.parse(response.content)
+       var timeArray = data[currMonth][currDate-1].times;
+
+       var displayPrayer = '';
+
+       for (var i=0; i < 6; i++){
+         var time = moment(timeArray[i]).format('HH:mm');
+         displayPrayer += `${Object.keys(PRAYER)[i]}: ${time}`
+       }
+
+     that.setState({
+       prayer: displayPrayer
+     })
+   }
+    })
+
+    // const PRAYER = {
+    //   'Subuh': 0,
+    //   'Syuruk': 1,
+    //   'Zuhur': 2,
+    //   'Asar': 3,
+    //   'Maghrib': 4,
+    //   'Isyak': 5
+    // }
+    // request({
+    //   url: `https://raw.githubusercontent.com/ruqqq/prayertimes-database/master/data/SG/1/2017.json`,
+    //   json: true
+    // }, (error, response, body) => {
+    //
+    //   if (!error && response.statusCode === 200){
+    //     var timeArray = body[currMonth][currDate-1].times;
+    //
+    //     for (var i=0; i < 6; i++){
+    //       var time = moment(timeArray[i]).format('HH:mm');
+    //       console.log(`${Object.keys(PRAYER)[i]}: ${time}`)
+    //     }
+    //
+    //   } else {
+    //     console.log('Error!');
+    //   }
+    // });
+  }
+
+  render() {
+
+   return (
+     <div>
+       {/* <h1>{this.state.hijrah}</h1> */}
+       <h1>{this.state.prayer}</h1>
+     </div>
+   )
 
   }
 
