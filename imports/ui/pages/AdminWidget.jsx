4658bcc2f6f2faf56f10399f2fe3fa0d3b7eab44
tengkuhafidz <tengkuhafidz@gmail.com>
2017-01-20 12:22:51 +0800

    styled admin widget

--- imports/ui/pages/AdminWidget.jsx
+++ imports/ui/pages/AdminWidget.jsx
@@ -7,15 +7,18 @@ export default class AdminWidget extends React.Component {
   render() {
 
     return (
-      <div>
-        <LogoutBtn />
-        <ul>
-          <li>
-            <a href="/challengeForm" className="centreLeft formalFont white-text mainLink">
-            <i className="material-icons iconAlign">location_on</i>Add Challenge</a>
+      <div className="adminPanel">
+        <i className="fa fa-user-secret fa-2x iconAlign" aria-hidden="true"></i>
+        <div className="adminMenu" >
+          <LogoutBtn />
+          <ul>
+            <li>
+              <a href="/challengeForm" className=" formalFont white-text mainLink">
+              <i className="material-icons iconAlign">location_on</i>Add Challenge</a>
 
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     )
 
