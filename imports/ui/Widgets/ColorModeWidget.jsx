2497f1de45e523318fa23c5547d2eb9e11d8d7f9
tengkuhafidz <tengkuhafidz@gmail.com>
2017-01-25 09:52:17 +0800

    fix no green bg issue

--- imports/ui/Widgets/ColorModeWidget.jsx
+++ imports/ui/Widgets/ColorModeWidget.jsx
@@ -19,8 +19,8 @@ export default class ColorModeWidget extends React.Component {
 
 
   setBgColor(){
-        var colors = ["rgba(15,109,102, 0.8), rgba(15,109,102, 0.8)", "rgba(11,57,84, 0.9), rgba(11,57,84, 0.9)", "rgba(46,41,78, 0.9), rgba(46,41,78, 0.9)", "rgba(0,0,0, 0.8), rgba(0,0,0, 0.8)"];
-        var currentBgColor = parseInt(localStorage.getItem("bgColor")) % colors.length || 1
+        var colors = ["rgba(11,57,84, 0.9), rgba(11,57,84, 0.9)", "rgba(46,41,78, 0.9), rgba(46,41,78, 0.9)", "rgba(0,0,0, 0.8), rgba(0,0,0, 0.8)", "rgba(15,109,102, 0.8), rgba(15,109,102, 0.8)"];
+        var currentBgColor = parseInt(localStorage.getItem("bgColor")) % colors.length || 0
         var image = this.props.image;
 
         document.body.style.background = "linear-gradient("+colors[currentBgColor]+"), url('/bg/" + image + ".jpg') no-repeat center center fixed";
